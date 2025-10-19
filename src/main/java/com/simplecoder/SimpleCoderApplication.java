9cfe3516d0ac356c60561412a9c569f3eb739de6
cpf <cuipengfei2008@gmail.com>
2025-10-19 18:54:11 +0800

    新增配置文件和基础代码结构，包括 settings.local.json、pom.xml、SimpleCoderApplication.java 和 application.yml

---
+++ src/main/java/com/simplecoder/SimpleCoderApplication.java
@@ -0,0 +1,12 @@
+package com.simplecoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCoderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleCoderApplication.class, args);
+    }
+}
